61cf27241530ad760bd1549ff2ba7c5a34d4b673
zhanxianbin <zhanxianbin@magicBook>
2020-04-01 00:39:58 +0800

    f

--- boot-demo-lts/src/main/java/com/xinyue/business/JobClientReferenceBean.java
+++ boot-demo-lts/src/main/java/com/xinyue/business/JobClientReferenceBean.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.xinyue.framework.LtsClient;
+import com.xinyue.framework.lts.LtsClient;
 
 
 
